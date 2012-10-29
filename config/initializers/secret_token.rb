f83ab438fea8520d5750c36ad5e8db003679938a
Olivier Van Den Biggelaar <ovdbigge@gmail.com>
2012-10-29 15:36:56 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Letsband::Application.config.secret_token = 'db8a21ed357537519af02272e275cc9365440fdfbca1e956c04a877f987159822db21b9b6f9d1a4e21e8a262d605c03e77b48992d4a84a0dcf1270d3ee1da70d'
