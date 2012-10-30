2be2cfbe8887b1901730ccefe41b45bd6238a427
Olivier Van Den Biggelaar <ovdbigge@gmail.com>
2012-10-30 17:29:31 +0100

    Add railstutorial layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Letsband::Application.routes.draw do
+  get "users/new"
+
+  root to: 'static_pages#home'
+
+  match '/signup', to: 'users#new'
+  
+  match '/contact', to: 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+  end
+
+  describe "Contact page" do
+  end
+
+end
