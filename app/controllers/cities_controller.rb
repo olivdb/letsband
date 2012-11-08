8ec6e642bb95f791cccdcc71fb930fae61e6ad38
Olivier Van Den Biggelaar <ovdbigge@gmail.com>
2012-11-08 18:02:53 +0100

    use lower() in city name autocomplete db query to ensure compatibility with postgres (2)

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,6 @@
 class CitiesController < ApplicationController
 	def index
-		@cities = City.order(:name).where("name like lower(?)", "#{params[:term].lower}%").limit(5)
+		@cities = City.order(:name).where("LOWER(name) like ?", "#{params[:term].downcase}%").limit(5)
 		render json: @cities.map {|c| Hash[id: c.id, label: c.fullname, name: c.fullname]}
 	end
 end
