712be928d334ffb31d8f67f6866ce8b5b1816b34
olivdb <ovdbigge@gmail.com>
2012-11-22 15:43:16 +0100

    Add confirmation email

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_CONFIG = YAML.load_file("#{::Rails.root}/config/config.yml")[::Rails.env]
+APP_CONFIG = YAML.load_file("#{::Rails.root}/config/config.yml")[::Rails.env] if ::Rails.env == "production"
 
 # Initialize the rails application
 Letsband::Application.initialize!
