5c0fc0958372f9256090f9a5afaa6ccd65732e73
olivdb <ovdbigge@gmail.com>
2012-12-01 16:24:23 +0100

    mail sent only on new message

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,14 +3,22 @@ class Message < ActiveRecord::Base
   is_private_message
   validates :recipient_id, presence: true
 
+  before_save :check_if_new
   after_save :send_email_notification
 
+  def check_if_new
+    @was_a_new_record = new_record?
+    return true
+  end
   def send_email_notification
-  	if self.inviting_band_id
-  		MessageMailer.invitation_email(self).deliver
-  	else
-  		MessageMailer.message_email(self).deliver
-  	end
+    if @was_a_new_record
+    	if self.inviting_band_id
+    		MessageMailer.invitation_email(self).deliver
+    	else
+    		MessageMailer.message_email(self).deliver
+    	end
+    end
+    return true
   end
   
   # The :to accessor is used by the scaffolding,
