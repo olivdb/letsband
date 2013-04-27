36784c98ddcfa6e541c797f170b631776be5f1aa
olivdb <ovdbigge@gmail.com>
2013-04-28 00:31:28 +0200

    add www in front of host name in config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,7 +50,7 @@
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'letsband.org' }
+  config.action_mailer.default_url_options = { :host => 'www.letsband.org' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
@@ -60,7 +60,6 @@
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "mail.letsband.org",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: ENV["GMAIL_USERNAME"],
