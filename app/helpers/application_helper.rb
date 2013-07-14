cc4bda4e934b9d414919a89a13389827b8217554
loydvan <vdbiggelaar@gmail.com>
2013-07-14 19:53:11 +0200

    some changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Let's Band !"
+    base_title = "Let's band !"
     if page_title.empty?
       base_title
     else
